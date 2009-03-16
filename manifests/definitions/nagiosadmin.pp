1e5a7a7871cb4e797190f2a2b1822c1ef782442e
Mathieu Bornoz <mathieu.bornoz@camptocamp.com>
2009-03-16 14:28:31 +0100

    use vhost rather than fqdn

--- manifests/definitions/nagiosadmin.pp
+++ manifests/definitions/nagiosadmin.pp
@@ -1,27 +1,29 @@
 define nagios::webadmin ($ensure=present, $password) {
+
+  $vhost = $name
   
-  file {"/var/www/$name/private/nagios-htpasswd":
+  file {"/var/www/$vhost/private/nagios-htpasswd":
     ensure  => $ensure,
     owner   => root,
     group   => root,
     mode    => 644,
-    require => Apache::Vhost-ssl[$name],
+    require => Apache::Vhost-ssl[$vhost],
   }
 
   line {"set nagios admin password $password":
     line    => "nagiosadmin:${password}",
     ensure  => $ensure,
-    file    => "/var/www/$name/private/nagios-htpasswd",
-    require => File["/var/www/$name/private/nagios-htpasswd"],
+    file    => "/var/www/$vhost/private/nagios-htpasswd",
+    require => File["/var/www/$vhost/private/nagios-htpasswd"],
   }
 
-  file {"/var/www/$name/conf/nagios.conf":
+  file {"/var/www/$vhost/conf/nagios.conf":
     ensure  => $ensure,
     owner   => root,
     group   => root,
     mode    => 644,
     content => template("nagios/apache.conf.erb"),
-    require => File["/var/www/$name/private/nagios-htpasswd"],
+    require => File["/var/www/$vhost/private/nagios-htpasswd"],
     notify  => Exec["apache-graceful"],
   }
 
