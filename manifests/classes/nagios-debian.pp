ca7c44fdda5b4b522dddbb6d453a9c8812bb18f1
Mathieu Bornoz <mathieu.bornoz@camptocamp.com>
2009-03-16 15:27:49 +0100

    remove standard apache configuration

--- manifests/classes/nagios-debian.pp
+++ manifests/classes/nagios-debian.pp
@@ -1,1 +1,8 @@
-class nagios::debian inherits nagios::base {}
+class nagios::debian inherits nagios::base {
+
+  file {"/etc/apache2/conf.d/nagios3.conf":
+    ensure  => absent,
+    notify  => Exec["apache-graceful"],
+  }
+    
+}
