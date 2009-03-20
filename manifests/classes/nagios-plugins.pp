528dfc4fab97112b91cd125f27ca65c5f1d67c2d
Mathieu Bornoz <mathieu.bornoz@camptocamp.com>
2009-03-20 11:54:43 +0100

    added a class unofficial plugins

---
+++ manifests/classes/nagios-plugins.pp
@@ -0,0 +1,16 @@
+class nagios::plugins {
+
+  package {"liblwp-useragent-determined-perl":
+    ensure => present,
+  }
+
+  file {"/usr/lib/nagios/plugins/check_apachestatus":
+    ensure => present,
+    source  => "puppet:///nagios/plugins/check_apachestatus",
+    owner => root,
+    group => root,
+    mode => 755,
+    require => [Package["liblwp-useragent-determined-perl"], Package["nagios-plugins"]],
+  }
+
+}
