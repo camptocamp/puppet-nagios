d5d33d982265dd938b25c9cc032ec68cca9023bf
Mathieu Bornoz <mathieu.bornoz@camptocamp.com>
2009-07-10 09:00:12 +0200

    this file can be handled by another module like apache::auth::htpasswd

--- manifests/definitions/nagios-webadmin.pp
+++ manifests/definitions/nagios-webadmin.pp
@@ -6,7 +6,6 @@ define nagios::webadmin ($ensure=present, $vhost, $htpasswd_file) {
     group   => root,
     mode    => 644,
     content => template("nagios/apache.conf.erb"),
-    require => File["/var/www/$vhost/private/$htpasswd_file"],
     notify  => Exec["apache-graceful"],
   }
 
