144a5829fd78023a91559e7428d4cad7596d1cf2
Cedric Jeanneret <cedric.jeanneret@camptocamp.com>
2009-09-18 16:29:03 +0200

    define an nsca service.
    you define it on the _client_ (as an active check), and it's exported
    as a passive check on the _server_

---
+++ manifests/definitions/nagios-nsca-service.pp
@@ -0,0 +1,41 @@
+#
+# modules/nagios/manifests/definitions/nagios-nsca-service.pp 
+# manage distributed monitoring with nagios
+# Copyright (C) 2008 Mathieu Bornoz <mathieu.bornoz@camptocamp.com>
+# See LICENSE for the full license granted to you.
+#
+
+define nagios::nsca::service ($ensure=present,
+                              $service_description=false, 
+                              $export_for,
+                              $host_name=false,
+                              $contact_groups=false, 
+                              $use_active="generic-service-active", 
+                              $use_passive="generic-service-passive"
+                              ) {
+
+  nagios_service {$name:
+    ensure => $ensure,
+    use => $use_active,
+    host_name => $host_name ? {false => $hostname, default => $host_name},
+    check_command => $name,
+    tag => "nagios-${export_for}",
+    service_description => $service_description,
+    target => "$nagios_cfg_dir/services.cfg",
+    require => File["$nagios_cfg_dir/services.cfg"],
+    notify => Exec["nagios-reload"],
+  }
+
+  @@nagios_service {"@@$name on $hostname":
+    ensure => $ensure,
+    use => $use_passive,
+    host_name => $host_name ? {false => $hostname, default => $host_name},
+    tag => "nagios-${export_for}",
+    service_description => $service_description,
+    target => "$nagios_cfg_dir/services.cfg",
+    require => File["$nagios_cfg_dir/services.cfg"],
+    notify => Exec["nagios-reload"],
+    contact_groups => $contact_groups ? {false => undef, default => $contact_groups},
+  }
+
+}
