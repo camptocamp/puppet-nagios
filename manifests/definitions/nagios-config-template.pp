71a3cdd859fee311a330aea0939143ae839faae8
Cedric Jeanneret <cedric.jeanneret@camptocamp.com>
2009-09-18 16:25:04 +0200

    wrapper: create whatever template you want

---
+++ manifests/definitions/nagios-config-template.pp
@@ -0,0 +1,12 @@
+define nagios::config::template($ensure=present, $content, $conf_type) {
+
+  # set rights and owner
+  file {"${nagios_cfg_dir}/${conf_type}-${name}.cfg":
+    ensure => present,
+    owner  => root,
+    group  => root,
+    mode   => 0644,
+    content => template("nagios/template-all.erb"),
+    notify  => Exec["nagios-reload"],
+  }
+}
