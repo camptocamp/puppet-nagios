5a23f8c14fb65ddacc510088be39028384b7368d
Cedric Jeanneret <cedric.jeanneret@camptocamp.com>
2009-09-18 16:27:29 +0200

    define a local service to check

---
+++ manifests/definitions/nagios-local-service.pp
@@ -0,0 +1,23 @@
+#
+# modules/nagios/manifests/definitions/nagios-local-service.pp 
+# manage distributed monitoring with nagios
+# Copyright (C) 2008 Mathieu Bornoz <mathieu.bornoz@camptocamp.com>
+# See LICENSE for the full license granted to you.
+#
+
+define nagios::local::service ($ensure=present, $export_for=$fqdn, $service_description=false, $host_name=false, $contact_groups=false) {
+
+  nagios_service {$name:
+    ensure                => $ensure,
+    use                   => "generic-service-active",
+    host_name             => $host_name ? {false => $hostname, default => $host_name},
+    check_command         => $name,
+    tag                   => "nagios-${export_for}",
+    service_description   => $service_description ? {false => undef, default => $service_description},
+    contact_groups        => $contact_groups ? {false => undef, default => $contact_groups},
+    target                => "$nagios_cfg_dir/services.cfg",
+    require               => File["$nagios_cfg_dir/services.cfg"],
+    notify                => Exec["nagios-reload"],
+  }
+
+}
