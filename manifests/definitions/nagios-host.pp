4a4f7cfc1d0ff74d525ed651337fa0fd32bbe35b
Cedric Jeanneret <cedric.jeanneret@camptocamp.com>
2009-09-18 16:27:01 +0200

    define a simple host.

---
+++ manifests/definitions/nagios-host.pp
@@ -0,0 +1,22 @@
+#
+# modules/nagios/manifests/definitions/nagios-host.pp 
+# manage distributed monitoring with nagios
+# Copyright (C) 2008 Mathieu Bornoz <mathieu.bornoz@camptocamp.com>
+# See LICENSE for the full license granted to you.
+#
+
+define nagios::host ($ensure=present, $address=false, $nagios_alias=false, $hostgroups=false, $contact_groups=false) {
+
+  nagios_host {$name:
+    ensure     => $ensure,
+    use        => "generic-host-active",
+    address    => $address ? {false => $ipaddress, default => $address},
+    alias      => $nagios_alias ? {false => undef, default => $nagios_alias},
+    hostgroups => $hostgroups ? {false => undef, default => $hostgroups},
+    target     => "$nagios_cfg_dir/hosts.cfg",
+    notify     => Exec["nagios-reload"],
+    require    => File["$nagios_cfg_dir/hosts.cfg"],
+    contact_groups => $contact_groups ? {false => undef, default => $contact_groups},
+  }
+
+}
