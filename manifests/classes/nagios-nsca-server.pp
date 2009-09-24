ad1d2946afad3bc8a0b0f05e144a1442ea1ba6a6
Cedric Jeanneret <cedric.jeanneret@camptocamp.com>
2009-09-24 16:48:54 +0200

    cleaned configuration.

--- manifests/classes/nagios-nsca-server.pp
+++ manifests/classes/nagios-nsca-server.pp
@@ -17,7 +17,10 @@ class nagios::nsca::server {
 
   service {"nsca":
     ensure      => running,
+    enable      => true,
     hasrestart  => true,
+    hasstatus   => false,
+    pattern     => "/usr/sbin/nsca",
     require     => Package["nsca"],
   }
 
@@ -40,44 +43,15 @@ class nagios::nsca::server {
     notify        => Exec["nagios-reload"],
     require       => Class["nagios::base"],
   }
-  
-  case $operatingsystem {
-    Debian: {
-      # nsca package only post-configure stop, not start
-      exec {"install nsca init script":
-        command => "update-rc.d -f nsca remove && update-rc.d nsca defaults 99 16",
-        unless  => "test -f /etc/rc2.d/S99nsca",
-        require => Package["nsca"],
-      }
 
-      file {"/etc/nsca.cfg":
-        ensure  => present,
-        owner   => root,
-        group   => root,
-        mode    => 644,
-        content => template("nagios/nsca.cfg.erb"),
-        require => Package["nsca"],
-        notify  => Service["nsca"],
-      }
-    }
-    RedHat: {
-      exec {"install nsca init script":
-        command => "chkconfig nsca on",
-        unless  => "chkconfig --list | egrep -q 'nsca.*on'",
-        require => Package["nsca"],
-      }
-
-      file {"/etc/nagios/nsca.cfg":
-        ensure  => present,
-        owner   => root,
-        group   => root,
-        mode    => 644,
-        content => template("nagios/nsca.cfg.erb"),
-        require => Package["nsca"],
-        notify  => Service["nsca"],
-      }
-
-    }
-    default: { err ("operatingsystem $operatingsystem not yet implemented !") }
+  file {"/etc/nagios/nsca.cfg":
+    ensure  => present,
+    owner   => root,
+    group   => root,
+    mode    => 644,
+    content => template("nagios/nsca.cfg.erb"),
+    require => Package["nsca"],
+    notify  => Service["nsca"],
   }
+
 }
