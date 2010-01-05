674ba920c0c2e5c46c44308594055cfeb2b806eb
Marc Fournier <marc.fournier@camptocamp.com>
2010-01-05 09:32:15 +0100

    nagios: fixed wrong ensure param.

--- manifests/definitions/nagios-config-template.pp
+++ manifests/definitions/nagios-config-template.pp
@@ -2,7 +2,7 @@ define nagios::config::template($ensure=present, $content, $conf_type) {
 
   # set rights and owner
   file {"${nagios_cfg_dir}/${conf_type}-${name}.cfg":
-    ensure => present,
+    ensure => $ensure,
     owner  => root,
     group  => root,
     mode   => 0644,
