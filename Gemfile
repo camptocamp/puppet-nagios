3ac66b26a23b594c9b9fbdda6f073089992b07cc
Mickaël Canévet <mickael.canevet@camptocamp.com>
2014-06-06 14:54:12 +0200

    Fix unit tests

--- Gemfile
+++ Gemfile
@@ -1,6 +1,12 @@
 source 'https://rubygems.org'
 
-puppetversion = ENV['PUPPET_GEM_VERSION']
-gem 'puppet', puppetversion, :require => false
-gem 'puppet-lint'
-gem 'puppetlabs_spec_helper'
+group :development, :test do
+  gem 'puppetlabs_spec_helper', :require => false
+  gem 'puppet-lint',            :require => false
+end
+
+if puppetversion = ENV['PUPPET_GEM_VERSION']
+  gem 'puppet', puppetversion, :require => false
+else
+  gem 'puppet', :require => false
+end
--- spec/classes/nagios_spec.rb
+++ spec/classes/nagios_spec.rb
@@ -1,6 +1,9 @@
 require 'spec_helper'
 
 describe 'nagios' do
+  let :pre_condition do
+    "Exec { path => '/foo', }"
+  end
   let(:facts) {{
     :concat_basedir  => '/foo',
     :id              => 'root',
@@ -9,5 +12,7 @@ describe 'nagios' do
     :osfamily        => 'Debian',
     :path            => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
   }}
-  it { pending('rspec-puppet bug') { should compile.with_all_deps } }
+  it 'should compile' do
+    should compile.with_all_deps
+  end
 end
