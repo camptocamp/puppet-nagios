d7d6575f77e7a975d735c9b882af851d34475663
Raphaël Pinson <raphael.pinson@camptocamp.com>
2020-09-22 08:15:22 +0200

    Allow to purge nrpe classes

--- manifests/nrpe/server.pp
+++ manifests/nrpe/server.pp
@@ -11,33 +11,49 @@
 #   include nagios
 #   include nagios::nrpe::server
 #
-class nagios::nrpe::server {
+class nagios::nrpe::server (
+  Enum['present', 'absent'] $ensure = 'present',
+) {
   include ::nagios::params
+
+  case $ensure {
+    'present': {
+      $pkg_ensure = $ensure
+    }
+
+    default: {
+      $pkg_ensure = $::osfamily ? {
+        'RedHat' => 'absent',
+        'Debian' => 'purged',
+      }
+    }
+  }
 
   case $::osfamily {
     'Debian': {
       package {'nagios-nrpe-plugin':
-        ensure => present,
+        ensure => $pkg_ensure,
       }
     }
     'RedHat': {
       package {'nagios-plugins-nrpe':
-        ensure => present,
+        ensure => $pkg_ensure,
       }
     }
 
     default: { fail ("OS family ${::osfamily} not yet implemented !")}
   }
 
-  $get_tag = "nagios-${::nagios::nrpe_server_tag}"
+  if $ensure == 'present' {
+    $get_tag = "nagios-${::nagios::nrpe_server_tag}"
 
-  Nagios::Host   <<| tag == $get_tag |>>
-  Nagios_service <<| tag == $get_tag |>>
-  Nagios_command <<| tag == $get_tag |>>
-  File           <<| tag == $get_tag |>>
+    Nagios::Host   <<| tag == $get_tag |>>
+    Nagios_service <<| tag == $get_tag |>>
+    Nagios_command <<| tag == $get_tag |>>
+    File           <<| tag == $get_tag |>>
 
-  Nagios_host    { require => File[$nagios::params::resourcedir] }
-  Nagios_service { require => File[$nagios::params::resourcedir] }
-  Nagios_command { require => File[$nagios::params::resourcedir] }
-
+    Nagios_host    { require => File[$nagios::params::resourcedir] }
+    Nagios_service { require => File[$nagios::params::resourcedir] }
+    Nagios_command { require => File[$nagios::params::resourcedir] }
+  }
 }
